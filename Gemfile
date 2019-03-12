98d73bad570a6969f8479df8c46e54e49e43e047
IlinDmitry <339966289@mail.ru>
2019-03-12 14:59:51 +0300

    nokogiri

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source "https://rubygems.org"
 
 # Specify your gem's dependencies in ecwid_yandex.gemspec
 gemspec
+
+ruby "2.6.1"
+gem "nokogiri"
--- lib/ecwid_yandex.rb
+++ lib/ecwid_yandex.rb
@@ -1,6 +1,38 @@
 require "ecwid_yandex/version"
+require 'csv'
+require 'nokogiri'
+
 
 module EcwidYandex
-  class Error < StandardError; end
-  # Your code goes here...
+  extend self
+
+  class FileForReading
+    def initialize(name, ext)
+      @file = "#{Dir.pwd}/public/#{name}.#{ext}"
+    end
+  end
+
+  class CSVData < FileForReading
+
+    def read_csv
+      csv_text = File.read(@file)
+      csv = CSV.parse(csv_text, headers: true, col_sep: ';')
+      csv.each do |row|
+        p row.to_hash['sku']
+      end
+    end
+  end
+
+  class XMLdata < FileForReading
+
+  end
+
+  def start
+    csv = CSVData.new('order', 'csv')
+    csv.read_csv
+
+    xml = XMLdata.new('yandex','xml')
+
+  end
+  # csv.read_csv
 end
